3d7dd7fd722099d4e1da770dc0f8ac9004ab90ba
axelthepony27 <axeldgarciab@gmail.com>
2023-08-28 14:08:05 -0600

    Adds the Amount class in data.py.
    This commit adds the Amount class in the script data.py. This class
    abstracts the concept of an amount, with a quantity and a
    commodity, and methods to parse and format amounts correctly.

--- scripts/data.py
+++ scripts/data.py
@@ -1,32 +1,106 @@
 import datetime
+import decimal
+import re
+
+from moneyed import Money
+
+
+class Amount:
+    currency_dict = {
+        "USD": "$",
+    }
+
+    def __init__(self, quantity: decimal.Decimal | str | int, commodity: str, is_currency: bool = False):
+        self.quantity = decimal.Decimal(quantity)
+        self.commodity = commodity
+        self.is_currency = is_currency
+
+    def description(self):
+        return f"{self.quantity}    {self.commodity}"
+
+    @staticmethod
+    def get_symbol_from_name(name: str):
+        return Amount.currency_dict.get(name)
+
+    @staticmethod
+    def get_name_from_symbol(symbol: str):
+        for key, value in Amount.currency_dict.items():
+            if symbol == value:
+                return key
+
+        return "key doesn't exist"
+
+    @staticmethod
+    def parse(string: str):
+        pattern = r"[^a-zA-Z0-9\s.,\/@-]"
+        elements = string.split()
+        if len(elements) == 2:
+            return Amount(decimal.Decimal(elements[0]), elements[1])
+        else:
+            elements = re.split(pattern, string)
+            symbol = re.search(pattern, string)
+            commodity = Amount.get_name_from_symbol(symbol.group(0))
+            if elements[0] == '-':
+                quantity = decimal.Decimal(elements[1])
+                return Amount(-quantity, commodity, True)
+            else:
+                quantity = decimal.Decimal(elements[1])
+                return Amount(quantity, commodity, True)
+
+    def format(self):
+        if not self.is_currency:
+            return f"{self.quantity} {self.commodity}"
+        else:
+            if self.quantity >= 0:
+                return "{symbol}{quantity:,.2f}".format(symbol=Amount.get_symbol_from_name(self.commodity),
+                                                        quantity=self.quantity)
+            else:
+                return "-{symbol}{quantity:,.2f}".format(symbol=Amount.get_symbol_from_name(self.commodity),
+                                                         quantity=self.quantity.__abs__())
 
 
 class Posting:
 
-    def __init__(self, account: str, amount: float, commodity: str):
+    def __init__(self, account: str, quantity: Money):
         self.account = account
-        self.amount = amount
-        self.commodity = commodity
+        self.quantity = quantity
 
     def description(self):
-        return f"{self.account}    {self.commodity}{self.amount}"
+        return f"{self.account}    {self.quantity}"
 
 
 class Transaction:
 
-    def __init__(self, date: datetime.date, payee: str, postings: list[Posting]):
+    def __init__(self, date: datetime.date, payee: str, postings: list[Posting] = None):
         self.date = date
         self.payee = payee
         self.postings = postings
 
     def description(self):
         x = ""
-        for posting in self.postings:
-            x += posting.description() + "\n"
-        return f"{self.date}    {self.payee}, \n{x}"
+        if self.postings is not None:
+            for posting in self.postings:
+                x += posting.description() + "\n"
+            return f"{self.date}    {self.payee} \n{x}"
+        else:
+            return f"{self.date}    {self.payee} No postings"
 
 
-posting1 = Posting("Expenses:Food", 20, "$")
-posting2 = Posting("Assets:Checking", -20, "$")
+posting1 = Posting("Expenses:Food", Money(20, 'USD'))
+posting2 = Posting("Assets:Checking", Money(-20, "GBP"))
 txn = Transaction(datetime.date.today(), "Awesome Food Payee", [posting1, posting2])
-print(txn.description())
+txn2 = Transaction(datetime.date.today(), "Another awesome payee")
+
+# print(txn.description())
+# print()
+# print(Amount.get_symbol_from_name("USD"))
+# print(Amount.get_name_from_symbol("$"))
+# print(Amount.parse("-20 BTC").description())
+# print("-----")
+# amount = Amount.parse("$20")
+# print(amount.description())
+# amount2 = Amount.parse("-$20")
+# print(amount2.description())
+# print("-----")
+# amount3 = Amount(-20, "USD", True)
+# print(amount3.format())
